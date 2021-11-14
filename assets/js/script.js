c845cb11c6883b92d53c72b87c869fef5c1111b1
jiamata <jiamata57@gmail.com>
2021-11-13 21:56:04 -0500

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// var buttonEl = document.querySelector("#save-task");
+// console.log(buttonEl);
+
+// buttonEl.addEventListener("click", function() {
+//     alert("button clicked");
+//   });
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+// buttonEl.addEventListener("click", function() {
+//   var listItemEl = document.createElement("li");
+//   listItemEl.className = "task-item";
+//   listItemEl.textContent = "This is a new task.";
+//   tasksToDoEl.appendChild(listItemEl);
+// });
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
